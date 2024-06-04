58264a2b425e70dc019b55a1f0b464371c44db3b
oiviana <vianalucas80@gmail.com>
2024-06-04 19:47:16 -0300

    fix: components folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import Blackboard from "./components/Blackboard";
-import Professor from "./components/Professor";
-import Student from "./components/Student";
+import Blackboard from "./components/ui/Blackboard";
+import Professor from "./components/poo/Professor";
 import { FaInfoCircle } from "react-icons/fa";
+import InitialModal from "./components/ui/InitialModal";
 
 function App() {
   return (
-    <div className=" w-full max-w-[1210px] mx-auto p-2">
+    <div className=" w-full max-w-[1210px] mx-auto p-2 relative ">
       <Blackboard />
       <div className="flex flex-col md:flex-row md:justify-between my-4">
         <div className="mt-6">
@@ -16,10 +16,11 @@ function App() {
         <Professor />
       </div>
       <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-7 gap-3 ">
-        <Student />
+ 
      
   
       </div>
+      <InitialModal/>
     </div>
   );
 }
